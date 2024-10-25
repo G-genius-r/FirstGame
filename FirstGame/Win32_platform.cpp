c2510bae172c61d3fe7611ddedb536170fd629d7
Lera Mandrik <dratuti6272@gmail.com>
2024-10-26 00:18:46 +0300

    Game2.0 (Ready for lessons)

--- FirstGame/Win32_platform.cpp
+++ FirstGame/Win32_platform.cpp
@@ -1,5 +1,5 @@
 #include "utils.cpp"
-#include <Windows.h>
+#include <windows.h>
 
 global_variable bool running = true;
 
@@ -16,7 +16,7 @@ global_variable Render_State render_state;
 #include "renderer.cpp"
 #include "game.cpp"
 
-LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParm, LPARAM lParam) {
+LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	LRESULT result = 0;
 
 	switch (uMsg) {
@@ -31,7 +31,7 @@ LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParm, LPARAM lPar
 			render_state.width = rect.right - rect.left;
 			render_state.height = rect.bottom - rect.top;
 
-			int size = render_state.width * render_state.height*sizeof(unsigned int);
+			int size = render_state.width * render_state.height * sizeof(unsigned int);
 
 			if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
 			render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
@@ -46,7 +46,7 @@ LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParm, LPARAM lPar
 		} break;
 
 		default: {
-			result = DefWindowProc(hwnd, uMsg, wParm, lParam);
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
 		}
 	}
 	return result;
@@ -107,6 +107,9 @@ input.buttons[b].is_down = is_down;\
 						process_button(BUTTON_DOWN, VK_DOWN);
 						process_button(BUTTON_W, 'W');
 						process_button(BUTTON_S, 'S');
+						process_button(BUTTON_LEFT, VK_LEFT);
+						process_button(BUTTON_RIGHT, VK_RIGHT);
+						process_button(BUTTON_ENTER, VK_RETURN);
 					}
 				} break;
 
--- FirstGame/renderer.cpp
+++ FirstGame/renderer.cpp
@@ -13,8 +13,8 @@ draw_rect_in_pixels(int x0, int y0, int x1, int y1, u32 color) {
 	
 	x0 = clamp(0, x0, render_state.width);
 	x1 = clamp(0, x1, render_state.width);
-	y0 = clamp(0, y0, render_state.width);
-	y1 = clamp(0, y1, render_state.width);
+	y0 = clamp(0, y0, render_state.height);
+	y1 = clamp(0, y1, render_state.height);
 
 	for (int y = y0; y < y1; y++) {
 		u32* pixel = (u32*)render_state.memory + x0 + y*render_state.width;
@@ -43,7 +43,268 @@ draw_rect(float x, float y, float half_size_x, float half_size_y, u32 color) {
 	int y0 = y - half_size_y;
 	int y1 = y + half_size_y;
 
-	draw_rect_in_pixels( x0,  y0,  x1, y1, color);
+	draw_rect_in_pixels(x0,  y0, x1, y1, color);
+}
+
+const char* letters[][7] = {
+	 " 00",
+	"0  0",
+	"0  0",
+	"0000",
+	"0  0",
+	"0  0",
+	"0  0",
+
+	"000",
+	"0  0",
+	"0  0",
+	"000",
+	"0  0",
+	"0  0",
+	"000",
+
+	" 000",
+	"0",
+	"0",
+	"0",
+	"0",
+	"0",
+	" 000",
+
+	"000",
+	"0  0",
+	"0  0",
+	"0  0",
+	"0  0",
+	"0  0",
+	"000",
+
+	"0000",
+	"0",
+	"0",
+	"000",
+	"0",
+	"0",
+	"0000",
+
+	"0000",
+	"0",
+	"0",
+	"000",
+	"0",
+	"0",
+	"0",
+
+	" 000",
+	"0",
+	"0",
+	"0 00",
+	"0  0",
+	"0  0",
+	" 000",
+
+	"0  0",
+	"0  0",
+	"0  0",
+	"0000",
+	"0  0",
+	"0  0",
+	"0  0",
+
+	"000",
+	" 0",
+	" 0",
+	" 0",
+	" 0",
+	" 0",
+	"000",
+
+	" 000",
+	"   0",
+	"   0",
+	"   0",
+	"0  0",
+	"0  0",
+	" 000",
+
+	"0  0",
+	"0  0",
+	"0 0",
+	"00",
+	"0 0",
+	"0  0",
+	"0  0",
+
+	"0",
+	"0",
+	"0",
+	"0",
+	"0",
+	"0",
+	"0000",
+
+	"00 00",
+	"0 0 0",
+	"0 0 0",
+	"0   0",
+	"0   0",
+	"0   0",
+	"0   0",
+
+	"00  0",
+	"0 0 0",
+	"0 0 0",
+	"0 0 0",
+	"0 0 0",
+	"0 0 0",
+	"0  00",
+
+	"0000",
+	"0  0",
+	"0  0",
+	"0  0",
+	"0  0",
+	"0  0",
+	"0000",
+
+	" 000",
+	"0  0",
+	"0  0",
+	"000",
+	"0",
+	"0",
+	"0",
+
+	" 000 ",
+	"0   0",
+	"0   0",
+	"0   0",
+	"0 0 0",
+	"0  0 ",
+	" 00 0",
+
+	"000",
+	"0  0",
+	"0  0",
+	"000",
+	"0  0",
+	"0  0",
+	"0  0",
+
+	" 000",
+	"0",
+	"0 ",
+	" 00",
+	"   0",
+	"   0",
+	"000 ",
+
+	"000",
+	" 0",
+	" 0",
+	" 0",
+	" 0",
+	" 0",
+	" 0",
+
+	"0  0",
+	"0  0",
+	"0  0",
+	"0  0",
+	"0  0",
+	"0  0",
+	" 00",
+
+	"0   0",
+	"0   0",
+	"0   0",
+	"0   0",
+	"0   0",
+	" 0 0",
+	"  0",
+
+	"0   0 ",
+	"0   0",
+	"0   0",
+	"0 0 0",
+	"0 0 0",
+	"0 0 0",
+	" 0 0 ",
+
+	"0   0",
+	"0   0",
+	" 0 0",
+	"  0",
+	" 0 0",
+	"0   0",
+	"0   0",
+
+	"0   0",
+	"0   0",
+	" 0 0",
+	"  0",
+	"  0",
+	"  0",
+	"  0",
+
+	"0000",
+	"   0",
+	"  0",
+	" 0",
+	"0",
+	"0",
+	"0000",
+
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"0",
+
+	"   0",
+	"  0",
+	"  0",
+	" 0",
+	" 0",
+	"0",
+	"0",
+};
+
+internal void 
+draw_text(const char *text, float x, float y, float size, u32 color) {
+	float half_size = size * .5f;
+	float original_y = y;
+	
+
+	while (*text) {
+		if (*text != 32) {
+			const char** letter;
+			if (*text == 47) letter = letters[27]; 
+			else if (*text == 46) letter = letters[26];
+			else letter = letters[*text - 'A'];
+			float original_x = x;
+
+			for (int i = 0; i < 7; i++) {
+				const char* row = letter[i];
+				if (row != NULL) {
+					while (*row) {
+						if (*row == '0') {
+							draw_rect(x, y, half_size, half_size, color);
+						}
+						x += size;
+						row++;
+					}
+				}
+				y -= size;
+				x = original_x;
+			}
+		}
+		text++;
+		x += size * 6.f;
+		y = original_y;
+	}
 }
 
 internal void 
